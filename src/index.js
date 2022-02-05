e1893690590c36fb6fdbd5990b0a0de4cc866ee5
Edward <oluyaratosin123@gmail.com>
2022-02-05 13:10:21 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/style.css';
+import './App.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
